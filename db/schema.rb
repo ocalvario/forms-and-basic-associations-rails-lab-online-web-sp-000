f21d40ea3c6ebea1f04635334a6fda1c70138bd7
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-21 15:34:51 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112163315) do
+ActiveRecord::Schema.define(version: 20160112163306) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -20,12 +20,6 @@ ActiveRecord::Schema.define(version: 20160112163315) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "notes", force: :cascade do |t|
-    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
